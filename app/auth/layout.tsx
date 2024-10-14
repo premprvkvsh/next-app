d27fadcc665b301496fa93ca1819ebb81bf4231c
Prem <pprakash312003@gmail.com>
2024-10-15 03:05:39 +0530

    11

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,10 +1,10 @@
-import React from "react"
+import React, { ReactNode } from "react";
 
 export default function({children}: {
      children: React.ReactNode 
     }) {  
     return <div>
-            <div className="border-b p-1 text-center">
+            <div className="border-b p-1">
                 20% off for the next 3 days
             </div>
             {children}
