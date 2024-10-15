3c4aa2ad73d168e1db29fd0cd533e908a0554e2f
Prem <pprakash312003@gmail.com>
2024-10-15 23:49:31 +0530

    12

--- components/Signin.tsx
+++ components/Signin.tsx
@@ -1,4 +1,4 @@
-export default function Signin() {
+export const SigninComponent = function() {
     return <div className="h-screen flex justify-center flex-col">
         <div className="flex justify-center">
         <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
